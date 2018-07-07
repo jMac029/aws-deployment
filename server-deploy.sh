76d9ec44dc59412e8022b6d694283caa06099baa
James McMillan <jmcmillan29@gmail.com>
2018-07-07 16:56:39 -0700

    updated deploy script and added crontab file

--- server-deploy.sh
+++ server-deploy.sh
@@ -4,8 +4,9 @@ yum install httpd git stress -y
 cd /etc/httpd/conf
 cp httpd.conf httpdconfbackup.conf
 rm -rf httpd.conf
-wget https://s3-us-west-1.amazonaws.com/jmac-bucket/httpd.conf
+aws s3 cp s3://jmac-bucket/httpd.conf /etc/httpd/conf
 cd /var/www/html
 git clone https://github.com/jMac029/aws-website-deploy.git
+aws s3 cp s3://jmac-bucket/crontab /etc/
 service httpd start
 chkconfig httpd on
