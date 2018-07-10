65ea23d2391717ae7280069d7a7e0eda4bce66b6
James McMillan <jmcmillan29@gmail.com>
2018-07-09 21:26:42 -0700

    finished project ready for submission

--- server-deploy.sh
+++ server-deploy.sh
@@ -4,10 +4,10 @@ yum install httpd git stress -y
 cd /etc/httpd/conf
 cp httpd.conf httpdconfbackup.conf
 rm -rf httpd.conf
-wget https://s3-us-west-1.amazonaws.com/jmac-bucket/httpd.conf
+wget https://s3-us-west-2.amazonaws.com/jdm-mini-bucket/httpd.conf
 cd /var/www/html
 git clone https://github.com/jMac029/aws-website-deploy.git
 cd /etc
-wget https://s3-us-west-1.amazonaws.com/jmac-bucket/crontab
+wget https://s3-us-west-2.amazonaws.com/jdm-mini-bucket/crontab
 service httpd start
 chkconfig httpd on
